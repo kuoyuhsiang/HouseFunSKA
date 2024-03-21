92a8f3f5a09a1fdda5499ecc91a8943fe8b7c078
shawn.kuo29 <shawn.kuo@titansoft.com.sg>
2024-03-21 12:12:48 +0800

    Program.cs DI

--- HouseFunSKA/Program.cs
+++ HouseFunSKA/Program.cs
@@ -1,3 +1,8 @@
+using HouseFunSKA.Data;
+using HouseFunSKA.Services;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +11,10 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+string connectionString = "Data Source=TI03NB289;Initial Catalog=master;User ID=sa;Password=Shawn0316";
+builder.Services.AddDbContext<NorthwindContext>(options => options.UseSqlServer(connectionString));
+builder.Services.AddScoped<CustomerService>();
 
 var app = builder.Build();
 
