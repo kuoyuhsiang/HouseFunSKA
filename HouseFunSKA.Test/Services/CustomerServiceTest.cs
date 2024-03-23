8c8ec31f966127009205adcc4ef507c2a6b3ec61
shawn.kuo29 <shawn.kuo@titansoft.com.sg>
2024-03-23 11:19:53 +0800

    Revise mocked CustomerRepository

--- HouseFunSKA.Test/Services/CustomerServiceTest.cs
+++ HouseFunSKA.Test/Services/CustomerServiceTest.cs
@@ -1,5 +1,6 @@
-using HouseFunSKA.Data;
-using HouseFunSKA.Repositories;
+using HouseFunSKA.Repositories.Interface;
+using HouseFunSKA.Services;
+using HouseFunSKA.Services.Interface;
 using NSubstitute;
 using NUnit.Framework;
 
@@ -11,14 +12,17 @@ public class CustomerServiceTest
     [SetUp]
     public void SetUp()
     {
-        _northwindContext = Substitute.For<NorthwindContext>();
+        _customerRepository = Substitute.For<ICustomerRepository>();
+        _customerService = new CustomerService(_customerRepository);
     }
 
-    private NorthwindContext _northwindContext;
+    private ICustomerRepository _customerRepository;
+    private ICustomerService _customerService;
 
     [Test]
     public void GetCustomer_Should_Call_GetCustomerAsync_Method_In_CustomerRepository_Once()
     {
-        var customerRepository = new CustomerRepository(_northwindContext);
+        _customerService.GetCustomer();
+        _customerRepository.Received(1).GetCustomerAsync();
     }
 }
