31e7e42fb12150370a95d9d87ad4b766b25a82c7
shawn.kuo29 <shawn.kuo@titansoft.com.sg>
2024-03-23 11:40:57 +0800

    CustomerRepository expects to receive a request from CustomerService once

--- HouseFunSKA.Test/Services/CustomerServiceTest.cs
+++ HouseFunSKA.Test/Services/CustomerServiceTest.cs
@@ -1,3 +1,4 @@
+using HouseFunSKA.Models;
 using HouseFunSKA.Repositories.Interface;
 using HouseFunSKA.Services;
 using HouseFunSKA.Services.Interface;
@@ -24,5 +25,37 @@ public class CustomerServiceTest
     {
         _customerService.GetCustomer();
         _customerRepository.Received(1).GetCustomerAsync();
+    }
+
+    [Test]
+    public void GetCustomerById_Should_Call_GetCustomerByIdAsync_Method_In_CustomerRepository_Once()
+    {
+        var customer = new Customer();
+        _customerService.GetCustomerById(customer.CustomerID);
+        _customerRepository.Received(1).GetCustomerByIdAsync(customer.CustomerID);
+    }
+
+    [Test]
+    public void CreateCustomer_Should_Call_CreateCustomerAsync_Method_In_CustomerRepository_Once()
+    {
+        var customer = new Customer();
+        _customerService.CreateCustomer(customer);
+        _customerRepository.Received(1).CreateCustomerAsync(customer);
+    }
+
+    [Test]
+    public void UpdateCustomer_Should_Call_UpdateCustomerAsync_Method_In_CustomerRepository_Once()
+    {
+        var customer = new Customer();
+        _customerService.UpdateCustomer(customer, customer.CustomerID);
+        _customerRepository.UpdateCustomerAsync(customer, customer.CustomerID);
+    }
+
+    [Test]
+    public void DeleteCustomer_Should_Call_DeleteCustomerAsync_Method_In_CustomerRepository_Once()
+    {
+        var customer = new Customer();
+        _customerService.DeleteCustomer(customer.CustomerID);
+        _customerRepository.DeleteCustomerAsync(customer.CustomerID);
     }
 }
